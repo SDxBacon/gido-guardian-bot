3fc1a89d2457763b79473d44fdc2cf03e0399ba0
SDxBacon <z141134553@gmail.com>
2024-10-17 02:25:10 +0800

    refactor: 💡 loading key from env var of BOT_TOKEN in .env

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/SDxBacon/gido-guardian-bot/bot"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	bot.Token = "YOUR_CODE_GOES_HERE"
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	// get the bot token from the environment
+	token := os.Getenv("BOT_TOKEN")
+
+	bot.Token = token
 	bot.Run()
 }
