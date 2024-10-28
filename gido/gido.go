c3774a69855041b941a314a7759d60b33223fe40
SDxBacon <z141134553@gmail.com>
2024-10-28 13:53:18 +0800

    refactor: 💡 remodel gido package

--- gido/gido.go
+++ gido/gido.go
@@ -1,16 +1,61 @@
 package gido
 
-const FALLBACK_CURRENT_TICKET_NUMBER = "----"
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func GetCurrentWaitInfoMessage() (string, error) {
-	rawInfo, err := fetchWaitInfo()
+	waitInfo, err := fetchWaitInfo()
 	if err != nil {
 		return "", err
 	}
 
-	waitInfo, err := parseBody(rawInfo)
-	if err != nil {
-		return "", err
+	return waitInfo.toMessage(), nil
+}
+
+func WatchTicketNumber(s *discordgo.Session, i *discordgo.InteractionCreate, number int) {
+	tasksMutex.Lock()
+	defer tasksMutex.Unlock()
+
+	if task, exists := watchingTasks[i.Member.User.ID]; exists {
+		close(task.stopChan)
+		delete(watchingTasks, i.Member.User.ID)
 	}
-	return toMessage(waitInfo), nil
+
+	stopChan := make(chan struct{})
+	watchingTasks[i.Member.User.ID] = WatchingTask{stopChan: stopChan, targetNumber: number}
+
+	go watchTicketNumberRoutine(s, i, number, stopChan)
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: fmt.Sprintf("已開始監視 Ticket: %d", number),
+		},
+	})
+}
+
+func StopWatchTicket(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	tasksMutex.Lock()
+	defer tasksMutex.Unlock()
+
+	if task, exists := watchingTasks[i.Member.User.ID]; exists {
+		close(task.stopChan)
+		delete(watchingTasks, i.Member.User.ID)
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("<@%s> 已停止監視票號", i.Member.User.ID),
+			},
+		})
+	} else {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("<@%s> 您目前沒有正在監視的票號", i.Member.User.ID),
+			},
+		})
+	}
 }
--- gido/request.go
+++ gido/request.go
@@ -15,33 +15,35 @@ import (
 // Returns:
 //   - string: The response body from the HTTP GET request.
 //   - error: An error if the HTTP request fails, the status code is not OK, or reading the response body fails.
-func fetchWaitInfo() (string, error) {
-	// 獲取當前日期，格式為 YYYYMMDD
+func fetchWaitInfo() (WaitInfo, error) {
+	// get current date in YYYYMMDD format
 	currentDate := time.Now().Format("20060102")
-
-	// 獲取當前時間戳（毫秒）
+	// get current timestamp in milliseconds
 	timestamp := time.Now().UnixNano() / int64(time.Millisecond)
-
-	// 構建 URL
+	// construct the URL
 	url := fmt.Sprintf("http://vpn.weshine.com.tw:8088/WaitInfoWeb/WaitInfo_GIDOHandler.ashx?act=WaitInfo&DEP_CODE=吉哆火鍋百匯&Kind=a1&date=%s&_=%d", currentDate, timestamp)
 
-	// 發送 GET 請求
-	resp, err := http.Get(url)
+	// create an HTTP client with a timeout of 2 seconds
+	client := &http.Client{
+		Timeout: 2 * time.Second,
+	}
+	resp, err := client.Get(url)
 	if err != nil {
-		return "", fmt.Errorf("HTTP request failed: %v", err)
+		return WaitInfo{}, fmt.Errorf("HTTP request failed: %v", err)
 	}
 	defer resp.Body.Close()
 
-	// 檢查狀態碼
+	// check if the status code is OK
 	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("HTTP request returned status code %d", resp.StatusCode)
+		return WaitInfo{}, fmt.Errorf("HTTP request returned status code %d", resp.StatusCode)
 	}
 
 	// read response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf("failed to read response body: %v", err)
+		return WaitInfo{}, fmt.Errorf("failed to read response body: %v", err)
 	}
 
-	return string(body), nil
+	// parse the response body to WaitInfo
+	return parseWaitInfoFromResponse(string(body))
 }
