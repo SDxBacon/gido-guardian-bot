3439ad810e4b38768ee30502fb82c675d00f7839
SDxBacon <z141134553@gmail.com>
2025-06-15 12:45:16 +0800

    refactor: 💡 add ticket_tracker with options pattern to replace watchdog

--- gido/gido.go
+++ gido/gido.go
@@ -1,11 +1,5 @@
 package gido
 
-import (
-	"fmt"
-
-	"github.com/bwmarrin/discordgo"
-)
-
 func GetCurrentWaitInfoMessage() (string, error) {
 	waitInfo, err := fetchWaitInfo()
 	if err != nil {
@@ -15,47 +9,25 @@ func GetCurrentWaitInfoMessage() (string, error) {
 	return waitInfo.toMessage(), nil
 }
 
-func WatchTicketNumber(s *discordgo.Session, i *discordgo.InteractionCreate, number int) {
-	tasksMutex.Lock()
-	defer tasksMutex.Unlock()
+// func StopWatchTicket(s *discordgo.Session, i *discordgo.InteractionCreate) {
+// 	tasksMutex.Lock()
+// 	defer tasksMutex.Unlock()
 
-	if task, exists := watchingTasks[i.Member.User.ID]; exists {
-		close(task.stopChan)
-		delete(watchingTasks, i.Member.User.ID)
-	}
-
-	stopChan := make(chan struct{})
-	watchingTasks[i.Member.User.ID] = WatchingTask{stopChan: stopChan, targetNumber: number}
-
-	go watchTicketNumberRoutine(s, i, number, stopChan)
-
-	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Content: fmt.Sprintf("已開始監視 Ticket: %d", number),
-		},
-	})
-}
-
-func StopWatchTicket(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	tasksMutex.Lock()
-	defer tasksMutex.Unlock()
-
-	if task, exists := watchingTasks[i.Member.User.ID]; exists {
-		close(task.stopChan)
-		delete(watchingTasks, i.Member.User.ID)
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf("<@%s> 已停止監視票號", i.Member.User.ID),
-			},
-		})
-	} else {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf("<@%s> 您目前沒有正在監視的票號", i.Member.User.ID),
-			},
-		})
-	}
-}
+// 	if task, exists := watchingTasks[i.Member.User.ID]; exists {
+// 		close(task.stopChan)
+// 		delete(watchingTasks, i.Member.User.ID)
+// 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+// 			Type: discordgo.InteractionResponseChannelMessageWithSource,
+// 			Data: &discordgo.InteractionResponseData{
+// 				Content: fmt.Sprintf("<@%s> 已停止監視票號", i.Member.User.ID),
+// 			},
+// 		})
+// 	} else {
+// 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+// 			Type: discordgo.InteractionResponseChannelMessageWithSource,
+// 			Data: &discordgo.InteractionResponseData{
+// 				Content: fmt.Sprintf("<@%s> 您目前沒有正在監視的票號", i.Member.User.ID),
+// 			},
+// 		})
+// 	}
+// }
